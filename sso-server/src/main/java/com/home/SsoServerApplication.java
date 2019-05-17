5be45d02a51a61afcc1c63a069fe5632c1079a85
Sky <sky_bin@kingdee.com>
2019-05-17 14:43:23 +0800

    删掉 security 模块

---
+++ sso-server/src/main/java/com/home/SsoServerApplication.java
@@ -0,0 +1,17 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhailiang
+ *
+ */
+@SpringBootApplication
+public class SsoServerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SsoServerApplication.class, args);
+	}
+
+}
